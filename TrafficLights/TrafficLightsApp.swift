c73742262774c7b24201547623ba1e1e85914c07
NosovaDaria <kuleshovadasha@gmail.com>
2022-02-16 14:50:29 +0300

    Initial Commit

---
+++ TrafficLights/TrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsApp.swift
+//  TrafficLights
+//
+//  Created by Дарья Носова on 16.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
